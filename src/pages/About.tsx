6e0264b69fb3a6d2b67334bb83a3e54ea1dbdf43
FeyaBloom <feya.bloom.design@gmail.com>
2025-11-07 22:06:22 +0100

    Actualizar About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -563,7 +563,7 @@ const About: React.FC = () => {
 
     {/* Тексты */}
     <motion.g initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, delay: 0.8 }}>
-      <foreignObject x="60" y="70" width="120" height="100">
+      <foreignObject x="60" y="70" width="130" height="100">
         <div className="flex flex-col items-center text-left">
           <Brush className="w-6 h-6 md:w-8 md:h-8 text-violeta mb-2" />
           <p className="text-md md:text-lg font-serif text-foreground">Visual <br /> Storytelling</p>
@@ -572,7 +572,7 @@ const About: React.FC = () => {
     </motion.g>
 
     <motion.g initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, delay: 1.0 }}>
-      <foreignObject x="220" y="70" width="120" height="80">
+      <foreignObject x="220" y="70" width="130" height="100">
         <div className="flex flex-col items-center text-right">
           <Brain className="w-6 h-6 md:w-8 md:h-8 text-azul mb-2" />
           <p className="text-md md:text-lg font-serif text-foreground">Functional <br /> Design</p>
@@ -581,7 +581,7 @@ const About: React.FC = () => {
     </motion.g>
 
     <motion.g initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, delay: 1.2 }}>
-      <foreignObject x="140" y="280" width="120" height="80">
+      <foreignObject x="140" y="280" width="120" height="100">
         <div className="flex flex-col items-center text-center">
           <LeafyGreen className="w-6 h-6 md:w-8 md:h-8 text-sage mb-2" />
           <p className="text-md md:text-lg font-serif text-foreground">Ancestral <br /> Wisdom</p>
